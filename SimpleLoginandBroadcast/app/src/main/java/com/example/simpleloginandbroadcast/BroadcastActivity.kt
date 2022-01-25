4e101abbcf20541643ec7d5355c489c6e51a7244
priunnagru <priunnagru@gmail.com>
2022-01-25 17:16:32 -0500

    Added new activity for the broadcast page Username: admin Password: admin

---
+++ SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/BroadcastActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simpleloginandbroadcast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BroadcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_broadcast)
+    }
+}
--- SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/MainActivity.kt
+++ SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.simpleloginandbroadcast
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val inputUsername = findViewById<EditText>(R.id.inputUsername)
+        val inputPassword = findViewById<EditText>(R.id.inputPassword)
+
+        val intentBroadcast = Intent(this, BroadcastActivity::class.java)
+        buttonLogin.setOnClickListener {
+            if (inputUsername.text.toString() == "admin"
+                && inputPassword.text.toString() == "admin") {
+                startActivity(intentBroadcast)
+            }
+        }
     }
 }
