e627ff23a0b226cbaa20858cb0720c64d0f2926f
priunnagru <priunnagru@gmail.com>
2022-01-29 19:33:11 -0500

    Switched to messaging rather than just emojis Sends notifications when messages are sent

--- SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/BroadcastActivity.kt
+++ SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/BroadcastActivity.kt
@@ -5,10 +5,13 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -19,26 +22,32 @@ class BroadcastActivity : AppCompatActivity() {
 
     // Same as root and user_info [See MainActivity] but for the emojis
     // TODO: OPTIONAL change name to reflect text instead of emoji; shift + F6 refactors
-    lateinit var emojiRef: DatabaseReference
-    lateinit var emojiDS: DataSnapshot
+    lateinit var messageRef: DatabaseReference
+    lateinit var messageDS: DataSnapshot
+
+    var notificationId = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast)
 
-        emojiRef = MainActivity.rootRef.child("emoji_info")
-        emojiRef.addValueEventListener(object: ValueEventListener {
+        messageRef = MainActivity.rootRef.child("emoji_info")
+        messageRef.addValueEventListener(object: ValueEventListener {
             // Reminder: called when listener is attached and when data changes
             override fun onDataChange(snapshot: DataSnapshot) {
-                emojiDS = snapshot
-                // TODO: Add function call to broadcast notifications when the data is changed
-                    // TODO: Make sure no messages sent when attached only data changed [See Below]
+                messageDS = snapshot
+
+
+
                 // TODO: OPTIONAL Add update to UI to show messages on page if time allows
 
                 // Basic testing to see if attached or changed
                 if (attached) {
-                    println("\n\n\n\nattached not changed\n\n\n\n")
+                    //println("\n\n\n\nattached not changed\n\n\n\n")
                     attached = false
+                } else {
+                    val temp = messageDS.child("e${messageDS.childrenCount}").value.toString()
+                    sendNotification(temp)
                 }
             }
 
@@ -50,7 +59,7 @@ class BroadcastActivity : AppCompatActivity() {
         displayUsername()
 
         // setup the emoji buttons
-        setupEmoji()
+        setupMessages()
     }
 
     private fun displayUsername() {
@@ -60,27 +69,28 @@ class BroadcastActivity : AppCompatActivity() {
         textUsernameDisplay.text = temp
     }
 
-    // Currently just adds <e#, 1 or 2 or 3 and user> based on the button
-    // So can tell who sent it and which emoji was sent
-    // TODO: change to be text messages, and add user and message to DB on clicking "Send" button
-    private fun setupEmoji() {
-        val emoji1 = findViewById<Button>(R.id.buttonEmoji1)
-        val emoji2 = findViewById<Button>(R.id.buttonEmoji2)
-        val emoji3 = findViewById<Button>(R.id.buttonEmoji3)
+    // Currently just adds <user>:<message>
+    private fun setupMessages() {
+        val sendMessage = findViewById<Button>(R.id.buttonSendMessage)
+        val messageBox = findViewById<EditText>(R.id.editTextMessage)
 
-        emoji1.setOnClickListener {
-            var emojiCount = emojiDS.childrenCount
-            emojiRef.child("e${++emojiCount}").setValue("1 ${MainActivity.accountUser}")
+        sendMessage.setOnClickListener {
+            var messageCount = messageDS.childrenCount
+            val htmlMessage = "" + MainActivity.accountUser + ": " + messageBox.text
+
+            messageRef.child("e${++messageCount}").setValue(htmlMessage)
+
+            messageBox.text.clear()
         }
 
-        emoji2.setOnClickListener {
-            var emojiCount = emojiDS.childrenCount
-            emojiRef.child("e${++emojiCount}").setValue("2 ${MainActivity.accountUser}")
-        }
-
-        emoji3.setOnClickListener {
-            var emojiCount = emojiDS.childrenCount
-            emojiRef.child("e${++emojiCount}").setValue("3 ${MainActivity.accountUser}")
+        // this simply allows you to press done on the keyboard to send the message
+        messageBox.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                sendMessage.performClick()
+                true
+            } else {
+                false
+            }
         }
     }
 
@@ -112,5 +122,18 @@ class BroadcastActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun sendNotification(message : String) {
+        val id = getString(R.string.channel_messages_id)
+        var builder = NotificationCompat.Builder(this, id)
+            .setSmallIcon(R.drawable.common_full_open_on_phone)
+            .setContentTitle(message.substringBefore(":") + " broadcasted a message!")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId++, builder.build())
+        }
     }
 }
--- SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/MainActivity.kt
+++ SimpleLoginandBroadcast/app/src/main/java/com/example/simpleloginandbroadcast/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.simpleloginandbroadcast
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
@@ -56,6 +60,9 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        // create notification channel
+        createNotificationChannel()
+
         // setup the login form
         setupLogin()
     }
@@ -109,5 +116,23 @@ class MainActivity : AppCompatActivity() {
 
         message = "Failed to login."
         return false
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_messages)
+            val descriptionText = getString(R.string.channel_messages_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val id = getString(R.string.channel_messages_id)
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
